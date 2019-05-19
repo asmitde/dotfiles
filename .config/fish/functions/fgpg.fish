8fff55de3b39a670e85bac4d63ad6995d719b9fb
Asmit De <asmit.de@sifive.com>
2019-05-18 23:30:11 -0700

    [Fixed #2] fgpg function updated. Cause: The GPG_TTY variable was not being exported globally. Fix: Added -g flag to the set commands.

--- .config/fish/functions/fgpg.fish
+++ .config/fish/functions/fgpg.fish
@@ -6,12 +6,12 @@
 
 function fgpg
     
-    set -x GPG_TTY (tty)
+    set -xg GPG_TTY (tty)
 
     if not test -e ~/.gnupg/S.gpg-agent
         gpg-agent --daemon --options ~/.gnupg/gpg-agent.conf
     end
 
-    set -x GPG_AGENT_INFO $HOME/.gnupg/S.gpg-agent:0:1
+    set -xg GPG_AGENT_INFO $HOME/.gnupg/S.gpg-agent:0:1
 
 end
